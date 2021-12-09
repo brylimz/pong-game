9132d48c3abf20491c637bcbe2e0741b43841f0e
Bryan`Y. Limz <superuser@brylimz.com>
2021-12-10 04:58:32 +0700

    need sleep

--- main.py
+++ main.py
@@ -1,32 +1,19 @@
 from turtle import Screen, Turtle
+from paddle import Paddle
+
 screen = Screen()
 screen.bgcolor("black")
 screen.setup(width=800, height=600)
 screen.title("Pong")
 screen.tracer(0)
 
-paddle = Turtle()
-paddle.shape("square")
-paddle.color("white")
-paddle.shapesize(stretch_wid=5, stretch_len=1)
-paddle.penup()
-paddle.goto(350, 0)
-
-
-def go_up():
-    new_y = paddle.ycor() + 20
-    paddle.goto(paddle.xcor(), new_y)
-
-
-def go_down():
-    new_y = paddle.ycor() - 20
-    paddle.goto(paddle.xcor(), new_y)
-
+r_paddle = Paddle((350, 0))
+l_paddle = Paddle((-350, 0))
 
 game_is_on = True
 while game_is_on:
     screen.update()
 
-screen.listen()
-screen.onkey(go_up, "Up")
-screen.onkey(go_down, "Down")
+screen.exitonclick()
+# screen.onkey(go_up, "Up")
+# screen.onkey(go_down, "Down")
