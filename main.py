bd7c022202ee84cb486222c91015589b56af9902
Bryan`Y. Limz <superuser@brylimz.com>
2021-12-10 09:42:35 +0700

    ball class and make the ball move

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from turtle import Screen, Turtle
 from paddle import Paddle
+from ball import Ball
+import time
+
 
 screen = Screen()
 screen.bgcolor("black")
@@ -9,6 +12,7 @@ screen.tracer(0)
 
 r_paddle = Paddle((350, 0))
 l_paddle = Paddle((-350, 0))
+ball = Ball()
 
 screen.listen()
 screen.onkey(r_paddle.go_up, "Up")
@@ -21,6 +25,9 @@ screen.onkey(l_paddle.go_down, "s")
 
 game_is_on = True
 while game_is_on:
+    time.sleep(0.1)
     screen.update()
+    ball.move()
+
 
 screen.exitonclick()
