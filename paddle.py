fd0e05f5a3e648f464f2c936064344239b45a341
Bryan`Y. Limz <superuser@brylimz.com>
2021-12-10 09:32:46 +0700

    paddle class and create the second paddle

--- paddle.py
+++ paddle.py
@@ -10,11 +10,10 @@ class Paddle(Turtle):
         self.penup()
         self.goto(position)
 
-    # def go_up():
-    #     new_y = self.ycor() + 20
-    #     self.goto(self.xcor(), new_y)
-    #
-    # def go_down():
-    #     new_y = self.ycor() - 20
-    #     self.goto(self.xcor(), new_y)
-    #
+    def go_up(self):
+        new_y = self.ycor() + 20
+        self.goto(self.xcor(), new_y)
+
+    def go_down(self):
+        new_y = self.ycor() - 20
+        self.goto(self.xcor(), new_y)
